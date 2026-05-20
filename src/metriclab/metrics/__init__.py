5d7d932839d1a91b73c999b9956d610123d88e4f
Thibaut Durand <durand.tibo@gmail.com>
2026-05-20 08:50:49 -0700

    Package structure

---
+++ src/metriclab/metrics/__init__.py
@@ -0,0 +1,1 @@
+r"""Contain stateful metrics."""
---
+++ src/metriclab/results/__init__.py
@@ -0,0 +1,5 @@
+r"""Immutable containers that store computed metric values.
+
+Result objects expose a consistent API to combine partial results and
+export aggregates as dictionaries.
+"""
